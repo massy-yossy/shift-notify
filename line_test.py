2c5e5b53561279a53273395f900a508c7236f57d
Masashi Yoshimoto <139942115+massy-yossy@users.noreply.github.com>
2026-01-26 22:57:43 +0900

    Update line_test.py

--- line_test.py
+++ line_test.py
@@ -2,8 +2,8 @@ import requests
 import os
 import json
 
-CHANNEL_ACCESS_TOKEN = os.getenv("UHEBH46clWWCzcKGu/NMHuqEOktnxzHdG/ABvj7xZYgUm5Oe/2kTKVhVuvy82P7eE97owV3mLhaFBw5vqp0BYCRkffDVaU/pLu/zENvXiDMhfMWPPD0KAMeWtIjfJeIZ09aK28G/Uy30cAn9suzvFQdB04t89/1O/w1cDnyilFU=")
-USER_ID = os.getenv("Ubaa71a296fd5f09b4488822497a3d1fb")
+CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_TOKEN")
+USER_ID = os.getenv("LINE_USER_ID")
 
 def send_line(message):
     url = "https://api.line.me/v2/bot/message/push"
